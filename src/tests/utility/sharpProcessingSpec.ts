f28eafbc93ae50bfb3e52f6ae3ad47bf157f776e
Mohamed Ayman <mohikel6@gmail.com>
2022-07-17 02:09:59 +0200

    added comments to sharpProcessSpec.ts

--- src/tests/utility/sharpProcessingSpec.ts
+++ src/tests/utility/sharpProcessingSpec.ts
@@ -2,9 +2,11 @@ import processImage from "../../utility/sharpProcessing";
 import fs from "fs";
 import path from "path";
 
+// testing image processing
 describe("Image processing tests", (): void => {
   it("is image processing adding a certain file or not", (): void => {
     if (
+      // checking if a file is already saved
       fs.existsSync(
         path.join(
           "assets",
@@ -14,6 +16,7 @@ describe("Image processing tests", (): void => {
       )
     ) {
       fs.unlinkSync(
+        // deleting it if it exists
         path.join(
           "assets",
           "processed_images",
@@ -21,7 +24,7 @@ describe("Image processing tests", (): void => {
         )
       );
     }
-    processImage("encenadaport", "400", "400");
+    processImage("encenadaport", "400", "400"); // calling the function processImage to process the image and create the file
     setTimeout((): void => {
       expect(
         fs.existsSync(
@@ -31,7 +34,7 @@ describe("Image processing tests", (): void => {
             "encenadaport_processed_400_400.jpg"
           )
         )
-      ).toBe(true);
+      ).toBe(true); // testing if it exists ot not
     }, 100);
   });
 });
